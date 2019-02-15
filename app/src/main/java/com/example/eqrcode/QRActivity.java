56274823c78036a3786d97363fcd2e812ab8a67a
blossomyyh <blossomyyh@163.com>
2019-02-15 15:32:40 +0800

    textureView

--- app/src/main/java/com/example/eqrcode/QRActivity.java
+++ app/src/main/java/com/example/eqrcode/QRActivity.java
@@ -1,14 +1,36 @@
 package com.example.eqrcode;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dynamsoft.barcode.BarcodeReader;
+import com.dynamsoft.barcode.EnumBarcodeFormat;
+import com.dynamsoft.barcode.PublicRuntimeSettings;
+import com.example.eqrcode.utils.DBRCache;
+
+import java.security.spec.ECField;
+
 public class QRActivity extends AppCompatActivity {
+
+    private BarcodeReader mBarcodeReader;
+    private DBRCache mDBRCache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        try {
+            mBarcodeReader = new BarcodeReader("t0068MgAAACm/O50JQCeJC5TJTNpXrUs4Do3MPzQxK0CvQvCGslylduMz/icYA3lAmVbE7NYhTFM60BRpW3QUav1sP6MWdZo=");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mDBRCache = DBRCache.get(this);
+        mDBRCache.put("linear", "1");
+
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.qrcode_container, QRFragment.newInstance())
@@ -16,6 +38,24 @@ public class QRActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        try{
+            int nBarcodeFormat =0;
+            nBarcodeFormat = nBarcodeFormat| EnumBarcodeFormat.BF_QR_CODE;
 
+            PublicRuntimeSettings runtimeSettings =  mBarcodeReader.getRuntimeSettings();
+            runtimeSettings.mBarcodeFormatIds = nBarcodeFormat;
+            mBarcodeReader.updateRuntimeSettings(runtimeSettings);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public BarcodeReader getMainBarcdoeReader(){
+        return mBarcodeReader;
+    }
 }
--- app/src/main/java/com/example/eqrcode/QRFragment.java
+++ app/src/main/java/com/example/eqrcode/QRFragment.java
@@ -1,19 +1,31 @@
 package com.example.eqrcode;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.eqrcode.utils.AutoFitTextureView;
+
 public class QRFragment extends Fragment implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private QRActivity mQRActivity;
     /**
      * Conversion from screen rotation to JPEG orientation.
      */
@@ -31,7 +43,7 @@ public class QRFragment extends Fragment implements ActivityCompat.OnRequestPerm
     /**
      * Tag for the {@link Log}.
      */
-    private static final String TAG = "Camera2BasicFragment";
+    private static final String TAG = "QRFragment";
 
     /**
      * Camera state: Showing camera preview.
@@ -68,11 +80,98 @@ public class QRFragment extends Fragment implements ActivityCompat.OnRequestPerm
      */
     private static final int MAX_PREVIEW_HEIGHT = 1080;
 
+
+    /**
+     * ID of the current {@link CameraDevice}.
+     */
+    private String mCameraId;
+
+    /**
+     * An {@link AutoFitTextureView} for camera preview.
+     */
+    private AutoFitTextureView mTextureView;
+
+    /**
+     * A {@link CameraCaptureSession } for camera preview.
+     */
+    private CameraCaptureSession mCaptureSession;
+
+    /**
+     * A reference to the opened {@link CameraDevice}.
+     */
+    private CameraDevice mCameraDevice;
+
+    /**
+     * The {@link Size} of camera preview.
+     */
+    private Size mPreviewSize;
+
     /**
      * {@link TextureView.SurfaceTextureListener} handles several lifecycle events on a
      * {@link TextureView}.
+     *
+     * <p>A TextureView can be used to display a content stream. Such a content
+     * stream can for instance be a video or an OpenGL scene. The content stream
+     * can come from the application's process as well as a remote process.</p>
+     *
+     * <p>TextureView can only be used in a hardware accelerated window. When
+     * rendered in software, TextureView will draw nothing.</p>
+     *
+     * <p>Unlike {@link SurfaceView}, TextureView does not create a separate
+     * window but behaves as a regular View. This key difference allows a
+     * TextureView to be moved, transformed, animated, etc. For instance, you
+     * can make a TextureView semi-translucent by calling
+     * <code>myView.setAlpha(0.5f)</code>.</p>
+     *
+     * <p>Using a TextureView is simple: all you need to do is get its
+     * {@link SurfaceTexture}. The {@link SurfaceTexture} can then be used to
+     * render content. The following example demonstrates how to render the
+     * camera preview into a TextureView:</p>
+     *
+     *
+     * <p>A TextureView's SurfaceTexture can be obtained either by invoking
+     * {@link #getSurfaceTexture()} or by using a {@link TextureView.SurfaceTextureListener}.
+     * It is important to know that a SurfaceTexture is available only after the
+     * TextureView is attached to a window (and {@link #onAttachedToWindow()} has
+     * been invoked.) It is therefore highly recommended you use a listener to
+     * be notified when the SurfaceTexture becomes available.</p>
+     *
+     * <p>It is important to note that only one producer can use the TextureView.
+     * For instance, if you use a TextureView to display the camera preview, you
+     * cannot use {@link #lockCanvas()} to draw onto the TextureView at the same
+     * time.</p>
+     *
+     * @see SurfaceView
+     * @see SurfaceTexture
      */
 
+    private final TextureView.SurfaceTextureListener mSurfaceTestureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    }
+
+    /**
+     *
+     * QRFragment initiate
+     *
+     * */
 
     public static QRFragment newInstance() {
 
@@ -123,12 +222,28 @@ public class QRFragment extends Fragment implements ActivityCompat.OnRequestPerm
     }
 
     public void setRequestCameraPermission() {
-
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+            new Confirma
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
+
+    // initiate TextureView.SurfaceTextureListener functions
+    /**
+     * Opens the camera specified by {@link QRFragment#mCameraId}.
+     */
+    private void openCamera (int width, int height){
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            req
+        }
+
     }
 
 
